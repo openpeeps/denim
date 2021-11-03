0dc4275a9e4c56c48bc2302ed4e9a53f4b90e1e6
George Lemon <georgelemon@protonmail.com>
2021-11-03 22:40:57 +0200

    Indentation and dirty fix
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/denim/napi/utils.nim
+++ src/denim/napi/utils.nim
@@ -1,12 +1,12 @@
 iterator enumerate*[T](open: openarray[T]): (int, T) =
-  var ct = 0
-  for elem in open:
-    yield (ct, elem)
-    inc ct
+    var ct = 0
+    for elem in open:
+        yield (ct, elem)
+        inc ct
 
 proc toUnchecked*[T](open: openarray[T]): ptr UncheckedArray[T] =
-  result = cast[ptr UncheckedArray[T]](alloc(sizeof(T) * open.len))
-  for i, elem in open.enumerate:
-    result[][i] = elem
+    result = cast[ptr UncheckedArray[T]](alloc(sizeof(T) * open.len))
+    for i, elem in open.enumerate:
+        result[][i] = elem
 
 
--- src/denim/utils.nim
+++ src/denim/utils.nim
@@ -1,30 +1,17 @@
-import os, strutils, macros
-from clymene/util import cmd
+from os import joinPath
 
-macro `?`* (a: bool, body: untyped): untyped =
-    # Create a macro for a short hand conditional
-    # value == expected ? "do something" ! "not yet"
-    # https://nim-lang.org/docs/sugar.html
-    let x = body[1]
-    let y = body[2]
-    result = quote:
-        if `a`: `x` else: `y`
-
-proc printf*(formatstr: cstring) {.importc: "printf", varargs, header: "<stdio.h>".}
+# macro `?`* (a: bool, body: untyped): untyped =
+#     # Create a macro for a short hand conditional
+#     # value == expected ? "do something" ! "not yet"
+#     # https://nim-lang.org/docs/sugar.html
+#     let x = body[1]
+#     let y = body[2]
+#     result = quote:
+#         if `a`: `x` else: `y`
 
 proc getNimPath*(knownPath: string = ""): string =
-    # Determine absolute path to Nim by executing "which nim"
-    # On MacOS, the result of "which nim" will return /usr/local/bin/nim
-    # which points to another symlink under Cellar directory
-    var slink = cmd("which", ["nim"]).strip()
-    # expand the path of the symlink
-    var exSlink = os.expandSymlink(slink)
-    # rm the symbolink name "nim" from path
-    slink = slink.replace("nim", "")
-    # normalize the symlink and get the absolute path of the main symlink
-    # right now, we can simply normalize the path and get directly into
-    # nim's "lib" source directory.
-    return os.normalizedPath(os.normalizedPath(slink & exSlink).replace("bin", "") & "/../nim/lib")
+    # Retrieve the path of the NIM library.
+    return "/Users/cristiangeorge/.choosenim/toolchains/nim-1.6.0/lib"
 
 proc getPath*(currentPath:string, toDirName:string, relative:string=""): string = 
     # Return path based on given current directory 
