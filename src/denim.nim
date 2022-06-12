375630ec1e497a7b8dbfb08318ea286e8dbf12ef
George Lemon <georgelemon@protonmail.com>
2022-06-13 01:28:47 +0300

    prepare for new klymene
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/denim.nim
+++ src/denim.nim
@@ -22,3 +22,14 @@ when isMainModule:
 
     if isCommand("new", args):          init.runCommand()
     elif isCommand("build", args):      compile.runCommand(args["<entry>"])
+
+# import klymene
+# import commands/[newCommand, buildCommand]
+
+# about:
+#     "Denim 🔥 Create powerful native NodeJS addons powered by Nim language."
+#     version "0.1.0"
+
+# commands:
+#     $ "new" <project>               "Create a new Nimble project (Invoke `nimble init`)"
+#     $ "build" <entry> --release     "Build Nim project to a native NodeJS addon"
